57cabaee8667951f5a3f7cfa37054a3fbed57be1
jyothipriyanka <jyothipriyanka1989@gmail.com>
2026-09-05 21:34:53 -0500

    Creation of the front

---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const functionApiUrl = '';
+const localFunctionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count =  response.count;
+        document.getElementById("visitor-count").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
+
